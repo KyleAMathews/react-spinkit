ea11e9d934da39eecf88c09cf04f7d6f893375dc
Kyle Mathews <mathews.kyle@gmail.com>
2014-08-11 12:55:34 -0700

    New release

---
+++ index.js
@@ -0,0 +1,9 @@
+var Spinner = require('../cjsx/index');
+var React = require('react');
+
+React.renderComponent(
+  React.DOM.div(null,
+    [
+      React.DOM.h2(null, "<Spinner spinnerName='three-bounce' />"),
+      Spinner({spinnerName: 'three-bounce'}),
+    ]), document.body);
